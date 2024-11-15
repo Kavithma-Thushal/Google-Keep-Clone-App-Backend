2a3256f0bafff2521db9fb4c1242628fbc030336
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-11-15 19:24:40 +0530

    Code Refactored 2

--- controller/NoteController.js
+++ controller/NoteController.js
@@ -3,7 +3,6 @@ const noteModel = require('../model/NoteModel');
 const save = async (req, res) => {
     const {title, content, color, username} = req.body;
     const newNote = new noteModel({title, content, color, username});
-
     try {
         await newNote.save();
         res.status(201).json({message: 'Note saved successfully!'});
